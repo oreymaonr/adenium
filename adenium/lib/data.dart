70f28cb77967ebc417fb259323946a009b2e7d2e
oreymaonr <ryanromeo10@gmail.com>
2024-06-09 20:18:36 +0200

    Done Basic functionality of the application

--- adenium/lib/data.dart
+++ adenium/lib/data.dart
@@ -1,30 +1,62 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:developer' as developer;
+
 import 'package:adenium/main.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PlantData {  
+  static const String green = "\x1B[32m";
+  static const String reset = "\x1B[0m";
+  static const String yellow = "\x1B[33m";
+  static const String red = "\x1B[31m";
+  static const String filelocation = "storage/emulated/0/FlutterPlantData/plantfile.txt";
+
+  Future<List<Plant>> get getplantData async => await _supplyPlist();
+
+  _supplyPlist() {
+    Future<List<Plant>> plantList = readJson();
+    return plantList;
+  }
+
+  //Writing the data to a json file
+  void writeJson(List<Plant> plants) async {    
+   
+    Directory dir = await getApplicationDocumentsDirectory();    
+    developer.log("${green}The directory path is :${dir.path}$reset");    
+    developer.log(green+filelocation+reset);
+    fileWritingPart(filelocation, plants);
+    
+  }
+
+  void fileWritingPart(String filename, List<Plant> plist) async {
+    File f = await File(filename).create(recursive: true);
+    final p = plist.map((plant) => plant.toJson()).toList();
+    f.writeAsString(jsonEncode(p));
+    developer.log("${green}writing is success$reset");
+  }
 
 
-class PlantData {
-  final List<Plant> plist = <Plant>[
-    Plant(
-      plantname: "adenium",
-      plantdetails: "Adenium obesum is grown as a houseplant in temperate and tropical regions." 
-      "Numerous hybrids have been developed. Adeniums are appreciated for their colorful flowers,"
-      " but also for their unusual, thick caudices. They can be grown for many years in a pot and "
-      "are commonly used for bonsai.One of the paired, follicular fruit of an Adenium species, "
-      "dehiscing to release seeds equipped with a double pappus (i.e. tuft of hairs at each end) for wind-dispersal"
-      "Because seed-grown plants are not genetically identical to the mother plant, desirable varieties are"
-      "commonly propagated by grafting. Genetically identical plants can also be propagated by cutting.",
-      plantdate: DateTime.now(),
-    ),
-    Plant(
-      plantname: "aster", 
-      plantdetails: 
-        "Like other members of the family Asteraceae, they have very small flowers collected together"
-        " into a composite flower head. Each single flower in a head is called a floret. In part due to"
-        " their abundance, along with being a generalist species, dandelions are one of the most vital"
-        " early spring nectar sources for a wide host of pollinators.",
-        plantdate: DateTime.now(),
-    ),
-  ];
-
-  List<Plant> get getplantData => plist;
+  Future<List<Plant>> readJson() async {
+    List<Plant> plantList = [];
+    var status = await Permission.storage.request();
+    developer.log("${yellow}status of the permission is :$status$reset");
+    try {      
+      File f = File(filelocation);      
+      String str = await f.readAsString();
+      final plantListJson = await jsonDecode(str);
+      for (var i = 0; i < plantListJson.length; i++) {
+        plantList.add(Plant(
+            plantname: plantListJson[i]["plantname"],
+            plantdetails: plantListJson[i]["plantdetails"],
+            plantdate: plantListJson[i]["plantdate"]));
+      }
+      developer.log("${green}read is success$reset");
+    // ignore: sdk_version_since
+    } on PathNotFoundException catch (_) {
+      developer.log("${red}exception caught while reading$reset");
+    }
+    return plantList;
+  }
 }
-
--- adenium/lib/main.dart
+++ adenium/lib/main.dart
@@ -1,25 +1,42 @@
 import 'package:adenium/data.dart';
 import 'package:flutter/material.dart';
 
+class Plant {
+  Plant(
+      {required this.plantname,
+      required this.plantdetails,
+      required this.plantdate});
 
-class Plant{
-  Plant({required this.plantname,required this.plantdetails,required this.plantdate});
   String plantname;
-  String plantdetails; 
-  DateTime plantdate;
+  String plantdetails;
+  String plantdate;
+
+  @override
+  String toString() {
+    return "plant_name :${plantname}plant_date :${plantdate}plant_details :$plantdetails";
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["plantname"] = plantname;
+    data["plantdetails"] = plantdetails;
+    data["plantdate"] = plantdate;
+    return data;
+  }
 }
+
 void main() {
-  //TODO: split the whole functionality to different dart
   runApp(const Adenium());
 }
 
-class Adenium extends StatelessWidget{
-  const Adenium({Key? key}) : super(key : key) ;
+class Adenium extends StatelessWidget {
+  const Adenium({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(    
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       title: "Adenium plant organiser",
+      debugShowCheckedModeBanner: false, 
       home: PlantOrg(),
     );
   }
@@ -33,28 +50,58 @@ class PlantOrg extends StatefulWidget {
 }
 
 class _PlantOrgState extends State<PlantOrg> {
-
   final TextEditingController _textEditingController1 = TextEditingController();
   final TextEditingController _textEditingController2 = TextEditingController();
-  late  DateTime plantBufferDate = DateTime.now();
+  late String plantBufferDate = DateTime.now().toString();
 
-  //input data
-  List<Plant>  plist = PlantData().getplantData; 
-  // TODO:externalize the testing input to a new dart file, maybe add a loading screen
+  List<Plant> plist = [];
+
   // TODO: to sort by date
 
   @override
-  Widget build(BuildContext context){
+  initState() {
+    plantDataLoad();
+    super.initState();
+  }
+
+  Future<void> plantDataLoad() async {
+    List<Plant> inputPlantList = await PlantData().getplantData;
+    _plantSorter(inputPlantList);
+    setState(() {
+      plist = inputPlantList;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue                
+              ),
+              child: Text("Plant Settings",style: TextStyle(fontSize: 40,fontFamily: "Noto Sans Korean"),),),
+            ListTile(
+              title: const Text("Download as Excel"),
+              onTap: () {
+                //TODO: Navigation
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
-        title:const Text("Plant Organiser"),
+        title: const Text("Plant Organiser"),
       ),
       body: ListView(
-        padding:const EdgeInsets.symmetric(vertical: 8.0),
-        children: plist.map((Plant plant){
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: plist.map((Plant plant) {
           return PlantOrgItem(
-            plant : plant,
-            onPlantOrgChange : _handlePlantOrgChange,
+            plant: plant,
+            onPlantOrgChange: _handlePlantOrgChange,
           );
         }).toList(),
       ),
@@ -65,204 +112,229 @@ class _PlantOrgState extends State<PlantOrg> {
       ),
     );
   }
-  _updatePlantItem(Plant plant,String updatedPlantname,String updatedPlantDetails,DateTime date){
-    setState(() {    
-      plist.remove(plant);   
-      plist.add(Plant(plantname: updatedPlantname, plantdetails: updatedPlantDetails,
-            plantdate: date));
+
+  _updatePlantItem(Plant plant, String updatedPlantname,
+      String updatedPlantDetails, String date) {
+    setState(() {
+      plist.remove(plant);
+      plist.add(Plant(
+          plantname: updatedPlantname,
+          plantdetails: updatedPlantDetails,
+          plantdate: date));
       _textEditingController1.clear();
       _textEditingController2.clear();
     });
+    _plantSorter(plist);
   }
-  _removePlantItem(Plant plant){
+
+  _removePlantItem(Plant plant) {
     setState(() {
       plist.remove(plant);
       _textEditingController1.clear();
-      _textEditingController2.clear();      
+      _textEditingController2.clear();
+      _plantSorter(plist);
     });
   }
-  _handlePlantOrgChange(Plant plant){
-    //your can update the plant details here 
-    //by turing iun another dilouge box
-    return  showDialog(      
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context){
-        return AlertDialog(
-          title: const Text("Modify plants"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _textEditingController1,
-                decoration: InputDecoration(hintText: plant.plantname),                
-              ),
-              TextField(
-                controller: _textEditingController2,
-                decoration: InputDecoration(hintText: plant.plantdetails),                
-              ),
-              ElevatedButton(
-                onPressed:() => _selectDate(context,plant) ,//_selectDate(context), 
-                child:const Text('show date picker'))
-            ],          
-          ),
-          actions: <Widget>[        
-            TextButton(
-              onPressed: (){   
-                Navigator.of(context).pop();             
-                _removePlantItem(plant);},                
-              child: const Text("Remove")
+
+  _handlePlantOrgChange(Plant plant) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Modify plants"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: _textEditingController1,
+                  decoration: InputDecoration(hintText: plant.plantname),
+                ),
+                TextField(
+                  controller: _textEditingController2,
+                  decoration: InputDecoration(hintText: plant.plantdetails),
+                ),
+                ElevatedButton(
+                    onPressed: () => _selectDate(context, plant),
+                    child: const Text('Set Date'))
+              ],
             ),
-            TextButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-                _updatePlantItem(
-                  plant, 
-                  _textEditingController1.text.isEmpty ? plant.plantname : _textEditingController1.text,
-                  _textEditingController2.text.isEmpty? plant.plantdetails:_textEditingController2.text,
-                  plantBufferDate
-                );                       
-              }, 
-              child: const Text("Update")
-            ),
-            TextButton(
-              onPressed: (){Navigator.pop(context);}, 
-              child: const Text("Cancel")
-            ),
-          ],
-        );
-      });
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _removePlantItem(plant);
+                  },
+                  child: const Text("Remove")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _updatePlantItem(
+                        plant,
+                        _textEditingController1.text.isEmpty
+                            ? plant.plantname
+                            : _textEditingController1.text,
+                        _textEditingController2.text.isEmpty
+                            ? plant.plantdetails
+                            : _textEditingController2.text,
+                        plantBufferDate);
+                  },
+                  child: const Text("Update")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")),
+            ],
+          );
+        });
   }
-  _addPlantItem(String name,String details){
+
+  _addPlantItem(String name, String details) {
     setState(() {
-      plist.add(Plant(plantname: name, plantdetails: details,plantdate: DateTime.now()));
+      plist.add(Plant(
+          plantname: name,
+          plantdetails: details,
+          plantdate: DateTime.now().toString()));
+      _plantSorter(plist);
     });
     _textEditingController1.clear();
     _textEditingController2.clear();
   }
-  Future<void> _selectDate(BuildContext context,Plant plant) async {    
-    DateTime? newSelectedDate = DateTime.now();
-    newSelectedDate = await showDatePicker(
+
+  _selectDate(BuildContext context, Plant plant) async {
+    DateTime? newSelectedDate = await showDatePicker(
       context: context,
-      initialDate: DateTime(2021,7,12),
+      initialDate: DateTime(2021, 7, 12),
       firstDate: DateTime(1999),
       lastDate: DateTime(2032),
     );
     setState(() {
-      plantBufferDate =newSelectedDate ??= DateTime.now();
+      plantBufferDate = newSelectedDate.toString();
     });
   }
 
-  _displayDialog(){
+  _displayDialog() {
     return showDialog(
-      context: context,
-      barrierDismissible: false, //user must tap button
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Add new Item"),
-          content: Column(
-            mainAxisAlignment: MainAxisAlignment.center,            
-            children: [
-              TextField(
-                controller: _textEditingController1,
-                decoration:  const InputDecoration(hintText: "add new plants"),
-              ),
-              TextField(
-                controller: _textEditingController2,
-                decoration:const InputDecoration(hintText: "details of the plant"),
-              )
+        context: context,
+        barrierDismissible: false, //user must tap button
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Add new Item"),
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _textEditingController1,
+                  decoration: const InputDecoration(hintText: "add new plants"),
+                ),
+                TextField(
+                  controller: _textEditingController2,
+                  decoration:
+                      const InputDecoration(hintText: "details of the plant"),
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    // TODO: to add date while adding plant
+                    _addPlantItem(_textEditingController1.text,
+                        _textEditingController2.text);
+                  },
+                  child: const Text("Add")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel"))
             ],
-            mainAxisSize: MainAxisSize.min,
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-                // TODO: to add date while adding plant               
-                _addPlantItem(
-                  _textEditingController1.text,
-                  _textEditingController2.text
-                );                  
-              },
-              child: const Text("Add")),
-            TextButton(
-              onPressed:(){
-                Navigator.pop(context);
-              },
-              child:const Text("Cancel") 
-            )
-          ],
-        );
-      });
+          );
+        });
   }
-
 }
 
+//function _plantSorter
+_plantSorter(List<Plant> unsortedPlantList) {
+  unsortedPlantList.sort((a, b) =>
+      (DateTime.parse(a.plantdate)).compareTo(DateTime.parse(b.plantdate)));
+  PlantData().writeJson(unsortedPlantList);
+}
+
+//function _dateTimeFormatter
+String _dateTimeFormatter(DateTime? datetime) {
+  //the function returns a string Date
+  final datetimeparsed = DateTime.parse(datetime.toString());
+  return "${datetimeparsed.day}-${datetimeparsed.month}-${datetimeparsed.year}";
+}
 
 class PlantOrgItem extends StatelessWidget {
   PlantOrgItem({
     Key? key,
     required this.plant,
     required this.onPlantOrgChange,
-    }) 
-    : super(key: ObjectKey(plant));
+  }) : super(key: ObjectKey(plant));
 
-  final Plant plant;
   // ignore: prefer_typing_uninitialized_variables
-  final onPlantOrgChange;
-
-  _dateTimeFormatter(DateTime? datetime){
-    //the function returns a string Date
-    final datetimeparsed = DateTime.parse(datetime.toString());
-    return "${datetimeparsed.day}-${datetimeparsed.month}-${datetimeparsed.year}";
-    
-  }
-  
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: (){
+      onTap: () {
         onPlantOrgChange(plant);
       },
-      leading: 
-      Container(
-        padding: const EdgeInsets.all(8.0),
-        child : const CircleAvatar(
-          child: Text('+'),
-        ),
-      ),            
+      leading:  Container(
+        padding: const EdgeInsets.symmetric(vertical:8.0),
+        child: const Icon(Icons.yard,size: 50,),
+      ),
       // title: Text(
       //   plant.plantname,
       //   style: _getTextStyle(plant.checked),
       // ),
       title: Row(
-        mainAxisSize: MainAxisSize.max,      
+        mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
-            child : 
-              Column(
-                children : [
-                  Text(
-                    plant.plantname,
-                    style:const TextStyle(fontSize: 30)),
-                  Text(_dateTimeFormatter(plant.plantdate)),
-                ]            
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),            
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),              
+              color: Colors.lightBlueAccent
             ),
-          ),         
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                plant.plantdetails,
-                textAlign: TextAlign.justify,
-              ),
-            )
-          ),          
+            child: Text(plant.plantname, style: const TextStyle(fontSize: 30)),
+            // Text(_dateTimeFormatter(DateTime.parse(plant.plantdate))),
+            // Text(plant.plantdate),
+          ),
+          Container(            
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0),            
+            child:
+                // Text(plant.plantname, style: const TextStyle(fontSize: 30)),
+                Text(_dateTimeFormatter(DateTime.parse(plant.plantdate)),style: const TextStyle(fontSize: 25),textAlign: TextAlign.right,),
+            // Text(plant.plantdate),
+          ),
         ],
-      ),  
+      ),
+      subtitle: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Colors.blueGrey),
+          ),
+        ),
+        child: Text(
+          plant.plantdetails,
+          textAlign: TextAlign.justify,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
+
+  final Plant plant;
+
+  final onPlantOrgChange;
 }
